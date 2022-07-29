e4bcc18e88b32f0788efe6fdfad5ac0be4f4e175
Sathya Narayana <54190045+SATHYA-NARAYANA@users.noreply.github.com>
2022-07-30 00:03:58 +0530

    Update yumconfigure.sh

--- bash script/yumconfigure.sh
+++ bash script/yumconfigure.sh
@@ -1,11 +1,11 @@
 cat >/etc/yum.repos.d/my.repo <<EOL
 [AppStream]
 name=AppStream
-baseurl= 
+baseurl=/run/media/root/RHEL-8-6-0-BaseOS-x86_64/AppStream
 gpgcheck=0
 
 [BaseOS]
 name=BaseOS
-baseurl= 
+baseurl=/run/media/root/RHEL-8-6-0-BaseOS-x86_64/BaseOS
 gpgcheck=0
 EOL
